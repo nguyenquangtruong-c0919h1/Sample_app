7fd2fe5e300e781f2beff4642b2ce9e48f590a6e
truong <quangtruong0412@gmail.com>
2020-06-02 10:16:23 +0700

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create,
+    :show]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,9 +23,53 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if User.find_by(id: params[:id]).destroy
+      flash[:success] = t ".user_detected"
+    else
+      flash[:danger] = t ".error_detected"
+    end
+    redirect_to users_url
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = t "users.edit.update_profile"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.edit.please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to(root_url) unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
